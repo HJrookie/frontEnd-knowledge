11fc0880b3aa24faa00dd31860a05cedb16147ba
HJrookie <1181400768@qq.com>
2021-12-29 18:03:02 +0800

    change

--- ts/test.js
+++
@@ -1,9 +0,0 @@
-function reverse(x) {
-    if (typeof x === 'number') {
-        return Number(x.toString().split('').reverse().join(''));
-    }
-    else if (typeof x === 'string') {
-        return x.split('').reverse().join('');
-    }
-}
-console.log(reverse(1241412));
